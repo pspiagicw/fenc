071fe52abca44deca2c86934b55cfe49c41e4e44
pspiagicw <pspiagicw@gmail.com>
2025-10-08 10:24:28 +0530

    feat: implemented binary operations in vm

--- vm/vm.go
+++ vm/vm.go
@@ -41,98 +41,181 @@ func (vm *VM) Run() {
 			vm.MulInt()
 		case code.SUB_INT:
 			vm.SubInt()
+		case code.DIV_INT:
+			vm.DivInt()
 		case code.LTE_INT:
 			vm.LteInt()
 		case code.LT_INT:
 			vm.LtInt()
+		case code.GTE_INT:
+			vm.GteInt()
+		case code.GT_INT:
+			vm.GtInt()
+		case code.ADD_FLOAT:
+			vm.AddFloat()
 		case code.SUB_FLOAT:
 			vm.SubFloat()
 		case code.MUL_FLOAT:
 			vm.MulFloat()
+		case code.DIV_FLOAT:
+			vm.DivFloat()
 		case code.LTE_FLOAT:
 			vm.LteFloat()
+		case code.LT_FLOAT:
+			vm.LtFloat()
+		case code.GTE_FLOAT:
+			vm.GteFloat()
+		case code.GT_FLOAT:
+			vm.GtFloat()
 		case code.OR_BOOL:
 			vm.OrBool()
+		case code.AND_BOOL:
+			vm.AndBool()
 		case code.NEQ:
 			vm.Neq()
+		case code.EQ:
+			vm.Eq()
+		case code.ADD_STRING:
+			vm.AddString()
 		default:
 			goreland.LogError("Invalid Op: %s", ins.OpCode)
 		}
 		vm.ip += 1
 	}
 }
-func (vm *VM) MulInt() {
-	l := vm.PopInt()
+
+func (vm *VM) AddFloat() {
+	r := vm.PopFloat()
+	l := vm.PopFloat()
+
+	vm.Push(object.CreateFloat(l.Value + r.Value))
+}
+func (vm *VM) AddString() {
+	r := vm.PopString()
+	l := vm.PopString()
+
+	vm.Push(object.CreateString(l.Value + r.Value))
+}
+func (vm *VM) Andbool() {
+	r := vm.PopBool()
+	l := vm.PopBool()
+
+	vm.Push(object.CreateBool(l.Value && r.Value))
+}
+func (vm *VM) DivFloat() {
+	r := vm.PopFloat()
+	l := vm.PopFloat()
+
+	vm.Push(object.CreateFloat(l.Value / r.Value))
+}
+func (vm *VM) DivInt() {
 	r := vm.PopInt()
+	l := vm.PopInt()
+
+	vm.Push(object.CreateInt((int)(l.Value / r.Value)))
+}
+func (vm *VM) GtFloat() {
+	r := vm.PopFloat()
+	l := vm.PopFloat()
+
+	vm.Push(object.CreateBool(l.Value > r.Value))
+}
+func (vm *VM) GteFloat() {
+	r := vm.PopFloat()
+	l := vm.PopFloat()
+
+	vm.Push(object.CreateBool(l.Value >= r.Value))
+}
+func (vm *VM) GtInt() {
+	r := vm.PopInt()
+	l := vm.PopInt()
+
+	vm.Push(object.CreateBool(l.Value > r.Value))
+}
+func (vm *VM) GteInt() {
+	r := vm.PopInt()
+	l := vm.PopInt()
+
+	vm.Push(object.CreateBool(l.Value >= r.Value))
+}
+func (vm *VM) LtFloat() {
+	r := vm.PopFloat()
+	l := vm.PopFloat()
+
+	vm.Push(object.CreateBool(l.Value < r.Value))
+}
+func (vm *VM) MulInt() {
+	r := vm.PopInt()
+	l := vm.PopInt()
 
 	vm.Push(object.CreateInt(l.Value * r.Value))
 }
 func (vm *VM) LtInt() {
-	l := vm.PopInt()
 	r := vm.PopInt()
+	l := vm.PopInt()
 
-	vm.Push(object.CreateBool(r.Value < l.Value))
+	vm.Push(object.CreateBool(l.Value < r.Value))
 }
 func (vm *VM) LteInt() {
-	l := vm.PopInt()
 	r := vm.PopInt()
+	l := vm.PopInt()
 
 	vm.Push(object.CreateBool(l.Value <= r.Value))
 }
 func (vm *VM) Neq() {
-	l := vm.Pop()
 	r := vm.Pop()
+	l := vm.Pop()
 
 	// TODO: Replace this with a more performant one
 	vm.Push(object.CreateBool(!reflect.DeepEqual(l, r)))
 }
 func (vm *VM) Eq() {
-	l := vm.Pop()
 	r := vm.Pop()
+	l := vm.Pop()
 
 	// TODO: Replace this with a more performant one
 	vm.Push(object.CreateBool(reflect.DeepEqual(l, r)))
 }
 func (vm *VM) AndBool() {
-	l := vm.PopBool()
 	r := vm.PopBool()
+	l := vm.PopBool()
 
 	vm.Push(object.CreateBool(l.Value && r.Value))
 }
 func (vm *VM) OrBool() {
-	l := vm.PopBool()
 	r := vm.PopBool()
+	l := vm.PopBool()
 
 	vm.Push(object.CreateBool(l.Value || r.Value))
 }
 func (vm *VM) AddInt() {
-	l := vm.PopInt()
 	r := vm.PopInt()
+	l := vm.PopInt()
 
 	vm.Push(object.CreateInt(l.Value + r.Value))
 }
 
 func (vm *VM) SubInt() {
-	l := vm.PopInt()
 	r := vm.PopInt()
+	l := vm.PopInt()
 
-	vm.Push(object.CreateInt(r.Value - l.Value))
+	vm.Push(object.CreateInt(l.Value - r.Value))
 }
 func (vm *VM) SubFloat() {
-	l := vm.PopFloat()
 	r := vm.PopFloat()
+	l := vm.PopFloat()
 
-	vm.Push(object.CreateFloat(r.Value - l.Value))
+	vm.Push(object.CreateFloat(l.Value - r.Value))
 }
 func (vm *VM) MulFloat() {
-	l := vm.PopFloat()
 	r := vm.PopFloat()
+	l := vm.PopFloat()
 
 	vm.Push(object.CreateFloat(r.Value * l.Value))
 }
 func (vm *VM) LteFloat() {
-	l := vm.PopFloat()
 	r := vm.PopFloat()
+	l := vm.PopFloat()
 
 	vm.Push(object.CreateBool(r.Value <= l.Value))
 }
@@ -164,6 +247,14 @@ func (vm *VM) PopBool() object.Bool {
 	v, ok := o.(object.Bool)
 	if !ok {
 		goreland.LogFatal("Expected object to be Bool, got %v", o)
+	}
+	return v
+}
+func (vm *VM) PopString() object.String {
+	o := vm.Pop()
+	v, ok := o.(object.String)
+	if !ok {
+		goreland.LogFatal("Expected object to be String, got %v", o)
 	}
 	return v
 }
